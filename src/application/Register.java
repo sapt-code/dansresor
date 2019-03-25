70f4ecbb9a6b95c9b530eab3b561a316e3e08abe
hsasys <his09hsa@student.lu.se>
2019-03-25 13:42:36 +0100

    test

--- src/application/Register.java
+++ src/application/Register.java
@@ -27,7 +27,7 @@ public class Register {
 		this.customers.remove(customer);
 	}
 	
-	//Antagligen onödig
+	//onödig
 	public Boolean constainsCustomer(String cNR) {
 		for (Customer c: customers) {
 			if (c.getCustomerNr().equals(cNR)) {
