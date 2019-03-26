efd10f9d8fc22a0aa37b503539ac17037fd03147
hsasys <his09hsa@student.lu.se>
2019-03-26 10:51:46 +0100

    createCustomer without allergies

--- src/application/CreateCustomerController.java
+++ src/application/CreateCustomerController.java
@@ -8,48 +8,67 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import application.Controller;
 
 public class CreateCustomerController {
-	
+
 	private Controller controller;
 
-    @FXML private TextField nameField;
-    @FXML private TextField personalField;
-    @FXML private TextField emailField;
-    @FXML private TextField phoneField;
-    @FXML private TextField addressField;
-    @FXML private TextField allergiesField;
-    @FXML private ChoiceBox<String> danceChoiceBox;
-    @FXML private Button createButton;
-    @FXML private Button cancelButton;
-    @FXML private Label warningLabel;
-    
-    public void initialize() {
-        danceChoiceBox.setItems(FXCollections.observableArrayList("Novice", "Intermediate", "Advanced"));
-    }
-    
-    
-    @FXML void cancelButton_click(ActionEvent event) {
-    	Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
-    }
+	@FXML
+	private TextField nameField;
+	@FXML
+	private TextField personalField;
+	@FXML
+	private TextField emailField;
+	@FXML
+	private TextField phoneField;
+	@FXML
+	private TextField addressField;
+	@FXML
+	private TextField allergiesField;
+	@FXML
+	private ChoiceBox<String> danceChoiceBox;
+	@FXML
+	private Button createButton;
+	@FXML
+	private Button cancelButton;
+	@FXML
+	private Label warningLabel;
 
-    @FXML
-    void createButton_click(ActionEvent event) {
-    	if (!nameField.getText().isEmpty() && !danceChoiceBox.getValue().isEmpty()) {
-    		controller.createCustomer("123", "344");
-    	} else {
-    		warningLabel.setText("Name and dance skill is required");
-    	}
-    	
-    	
-    	
-    }
+	public void initialize() {
+		danceChoiceBox.setItems(FXCollections.observableArrayList("Novice", "Intermediate", "Advanced"));
+	}
+
+	@FXML
+	void cancelButton_click(ActionEvent event) {
+		Stage stage = (Stage) cancelButton.getScene().getWindow();
+		stage.close();
+	}
+
+	@FXML
+	void createButton_click(ActionEvent event) {
+		if (!nameField.getText().isEmpty() && !danceChoiceBox.getValue().isEmpty()) {
+			String name = nameField.getText().trim();
+			String personal = personalField.getText().trim();
+			String email = emailField.getText().trim();
+			String phone = phoneField.getText().trim();
+			String address = addressField.getText().trim();
+			//ArrayList till allergies
+			String danceSkill = danceChoiceBox.getValue();
+			Customer c = controller.createCustomer(name, danceSkill);
+			c.setPersNr(personal);
+			c.setEmail(email);
+			c.setPhoneNr(phone);
+			c.setAddress(address);
+			
+		} else {
+			warningLabel.setText("Name and dance skill is required");
+		}
+
+	}
 
 	public void init(Controller contr) {
 		controller = contr;
-		
+
 	}
 
 }
